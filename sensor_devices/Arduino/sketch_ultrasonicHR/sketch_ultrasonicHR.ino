6a8dc79dc0270d3e7b04af4f9fe51fa74c8c611a
d-dw <dwang2@uw.edu>
2016-02-27 23:39:31 -0800

    Implemented battery-saving sleep routines to router and sensor libraries.

--- sensor_devices/Arduino/sketch_ultrasonicHR/sketch_ultrasonicHR.ino
+++ sensor_devices/Arduino/sketch_ultrasonicHR/sketch_ultrasonicHR.ino
@@ -5,10 +5,11 @@
 #include "SensorComm.h"
 #include "Timer.h"
 
-const byte type = 4;
+#define SENSOR_POWER_PIN 8
+
 XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x40DAF053);
 
-SensorComm scomm(2, 3, type, &addr64);
+SensorComm scomm(2, 3, TYPE_ULTRASONIC_HR, &addr64);
 
 UltrasonicHR ultrasonic(7);
 long RangeInCm;
@@ -22,6 +23,8 @@ void setup()
   Serial.begin(9600);
   scomm.begin();
   t100.reset();
+  pinMode(SENSOR_POWER_PIN, OUTPUT);
+  digitalWrite(SENSOR_POWER_PIN, HIGH);
   Serial.println(F("Initialized."));
 }
 
@@ -40,6 +43,14 @@ void loop()
       Serial.println(t);
       Serial.print(avgrange);//30-200
       Serial.println(F(" cm"));
+      if (scomm.getCommandAndClear() == COMMAND_SLEEP) {
+        // Turn off sensor
+        digitalWrite(SENSOR_POWER_PIN, LOW);
+        // Delay for CommandParam1 milliseconds
+        delay(scomm.getCommandParam1());
+        // Turn on sensor
+        digitalWrite(SENSOR_POWER_PIN, HIGH);
+      }
       avgrange = 0;
       count = 0;
     }
